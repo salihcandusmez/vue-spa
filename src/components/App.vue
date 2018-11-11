a763a456541f11c99a2fee148a1db777ab0cb4c1
salihcandusmez <salihcandusmez@gmail.com>
2018-11-11 22:35:14 +0300

    first commit

---
+++ src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container mt-5">
+    <Nav></Nav>
+    <Header></Header>
+    <Posts></Posts>  
+  </section>
+</template>
+
+<script>
+import Nav from './Nav'
+import Header from './Header'
+import Posts from './Posts'
+
+export default {
+  components: {
+    Nav,
+    Header,
+    Posts
+  }
+};
+</script>
+
+<style lang="scss">
+  @import "../styles/main.scss";
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card mr-4" style="width: 15rem; margin-bottom:1.6rem"> 
+    <div class="card-header">
+      <slot name="title"></slot>  
+    </div>
+    <div class="card-body h-100">
+      {{post.body}}
+    </div>
+    <div class="card-footer text-muted">
+      <a href="#" class="card-link">Card link</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props: [
+   'post'
+ ]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
